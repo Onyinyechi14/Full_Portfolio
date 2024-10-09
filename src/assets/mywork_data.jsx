23ac3cc9c9976157eb90da1467061fd31e34acf5
Onyinyechi14 <giftjulius070@gmail.com>
2024-10-09 12:19:39 +0100

    changed the url for my work

--- src/assets/mywork_data.jsx
+++ src/assets/mywork_data.jsx
@@ -10,25 +10,25 @@ const mywork_data = [
         w_no:1,
         w_name:"web design",
         w_img:project3,
-        w_ref:"http://127.0.0.1:5505/index.html",
+        w_ref:"https://onyinyechi14.github.io/Interior/",
     },
     {
         w_no:2,
         w_name:"web design",
         w_img:project4,
-        w_ref:"http://127.0.0.1:5505/index.html",
+        w_ref:"https://onyinyechi14.github.io/login-form/",
     },
     {
         w_no:3,
         w_name:"web design",
         w_img:project5,
-        w_ref:"http://127.0.0.1:5501/index.html",
+        w_ref:"https://onyinyechi14.github.io/shade/",
     },
     {
         w_no:4,
         w_name:"web design",
         w_img:project6,
-        w_ref:"http://127.0.0.1:5502/index.html",
+        w_ref:"https://onyinyechi14.github.io/store/",
     },
     {
         w_no:5,
@@ -39,7 +39,7 @@ const mywork_data = [
         w_no:6,
         w_name:"web design",
         w_img:project8,
-        w_ref:"http://127.0.0.1:5504/index.html",
+        w_ref:" https://onyinyechi14.github.io/my-weather-app/",
     },
 ]
 
