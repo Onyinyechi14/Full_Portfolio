44f99fed0a01674127d8ae80ddb421f3ea6f3ee9
Onyinyechi14 <giftjulius070@gmail.com>
2024-10-09 12:30:35 +0100

    added a new work

--- src/assets/mywork_data.jsx
+++ src/assets/mywork_data.jsx
@@ -2,15 +2,15 @@ import project3 from '../assets/project3.png'
 import project4 from '../assets/project4.png'
 import project5 from '../assets/project5.png'
 import project6 from '../assets/project6.png'
-import project7 from '../assets/project7.png'
+import traveleasy from '../assets/traveleasy.png'
 import project8 from '../assets/project8.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"web design",
-        w_img:project3,
-        w_ref:"https://onyinyechi14.github.io/Interior/",
+        w_img:traveleasy,
+        w_ref:"https://traveleasy-3lsn.onrender.com/",
     },
     {
         w_no:2,
@@ -33,7 +33,8 @@ const mywork_data = [
     {
         w_no:5,
         w_name:"web design",
-        w_img:project7
+        w_img:project3,
+        w_ref:"https://onyinyechi14.github.io/Interior/",
     },
     {
         w_no:6,
